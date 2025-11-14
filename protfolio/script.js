ae3d74626b7545c36b6672692cbe5e5bb4898317
Prince Parajuli <pricneparajuli@gmail.com>
2025-11-14 14:09:06 +0545

    Add files via upload

---
+++ protfolio/script.js
@@ -0,0 +1,8 @@
+var typed= new Typed('.text', {
+    strings: ['Full-Stack Developer', 'Data Science Enthusiast', 'Civil Engineer'],
+    typeSpeed: 100,     
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true, 
+    
+});
